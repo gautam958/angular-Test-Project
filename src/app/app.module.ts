29926751af2dfd2a4e40decccad13131665b7ed9
gautam958 <gautam958@gmail.com>
2022-07-02 18:12:49 +0800

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, MatCardModule],
+  declarations: [AppComponent, HelloComponent, HomeComponent],
+  bootstrap: [AppComponent],
+  exports: [MatCardModule],
 })
-export class AppModule { }
+export class AppModule {}
